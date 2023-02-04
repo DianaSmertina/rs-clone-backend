fcaec846fc820b46b2b8849f99dd6136aff149bc
DianaSmertina <am.dianasmertina@gmail.com>
2023-02-04 22:07:10 +0400

    fix: fix adding new user in remove db

--- backend/controller/user-controller.js
+++ backend/controller/user-controller.js
@@ -6,17 +6,14 @@ class UserController {
 
     }
 
-    async createNewUser(req, res) {
+    async createNewUser(req, res, next) {
         try {
             const name = await req.body.username;
             const password = await req.body.password;
-            console.log(req.body);
             // добаить проверку есть ли такой юзер
-            // const hash = bcrypt.hashSync(password, 7);
-            // console.log(hash)
-            const newUser = await db.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *', [name, password]);
-            this.stream.end();
-            res.json(newUser);
+            const hash = bcrypt.hashSync(password, 7);
+            const newUser = await db.query('INSERT INTO users(username, password) VALUES ($1, $2)', [name, hash]);
+            // res.json(newUser); не возвращает созданного юзера, разобраться
         } catch(e) {
             console.log(e);
             res.status(400).json({message: 'Registration error'});
